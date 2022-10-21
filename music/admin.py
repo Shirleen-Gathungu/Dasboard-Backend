58cb9c84152fe44efe4ac66230ca4ad3e1da16f4
Shirleen <gathungushirleen@gmail.com>
2022-10-21 10:15:40 +0300

    created models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Playlist
+from .models import Song
+from .models import Album
+from .models import Song
+from .models import Artist
 # Register your models here.
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name','profile_picture','playlist',)
+    search_fields = ('name','profile_picture','playlist',)
+admin.site.register(User,UserAdmin)
+
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('user','songs','album',)
+    search_fields = ('user','songs','album',)
+admin.site.register(Playlist,PlaylistAdmin)
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('title','artist','duration','album',)
+    search_fields = ('title','artist','duration','album',)
+admin.site.register(Song,SongAdmin)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name','profile_picture','album',)
+    search_fields = ('name','profile_picture','album',)
+admin.site.register(Artist,ArtistAdmin)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('song',)
+    search_fields = ('song',)
+admin.site.register(Album,AlbumAdmin)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,32 @@
+from operator import mod
+from pickle import SHORT_BINSTRING
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    name= models.CharField(max_length=20)
+    profile_picture=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    album = models.ForeignKey('Album',on_delete=models.CASCADE,related_name='artist_album')
+
+
+class Album(models.Model):
+    songs= models.ForeignKey('Song',on_delete=models.CASCADE,related_name='album_song')
+
+class Song(models.Model):
+    title= models.CharField(max_lenght=20)
+    artist= models.CharField(max_lenght=20)
+    duration= models.TimeField()
+    album = models.ForeignKey('Album',on_delete=models.CASCADE,related_name='song_album')
+
+class Playlist(models.Model):
+    user=models.ForeignKey('User',on_delete=models.CASCADE,related_name='album_song')
+    songs=models.ForeignKey('Songs',on_delete=models.CASCADE,related_name='album_song')
+    album=models.ForeignKey(Album,on_delete=models.CASCADE,related_name='album_song')
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    playlist = models.ForeignKey(Playlist,on_delete=models.CASCADE,related_name='user_playlist')
+   
